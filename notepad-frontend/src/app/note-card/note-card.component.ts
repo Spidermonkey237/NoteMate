f041a89cbbfeb28b4eaf197c66daa1d68cf2695a
Tchougang Yann Alias Spdkmk_237 <ewoyann@gmail.com>
2022-02-16 23:46:36 +0100

    I add delete button for the cards-components and degradation effect. I also add strictPropertyInitialization:true in tsconfig.json

--- notepad-frontend/src/app/note-card/note-card.component.ts
+++ notepad-frontend/src/app/note-card/note-card.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { style } from '@angular/animations';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.scss']
 })
+
+
 export class NoteCardComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('truncator', { static: true }) truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText', { static: true }) bodyText: ElementRef<HTMLElement>;
+  @ViewChild('noteP', { static: true }) noteP: ElementRef<HTMLElement>;
+
+  constructor(private renderer :Renderer2) {
+  }
 
   ngOnInit(): void {
+    let sytle = window.getComputedStyle(this.bodyText.nativeElement, null);
+    //let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+  }
+
+  ngAfterViewInit() {
+
+    if (this.noteP.nativeElement.scrollHeight > this.bodyText.nativeElement.clientHeight) {
+        this.renderer.setStyle(this.truncator.nativeElement, 'display','block');
+    }
+    else{
+      this.renderer.setStyle(this.truncator.nativeElement, 'display','none');
+    }
+
+
   }
 
 }
